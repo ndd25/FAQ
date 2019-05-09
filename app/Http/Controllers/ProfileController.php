70279e2a3fe2fedf11b4778148390467521c8170
Nigum Dave <ndd25@njit.edu>
2019-05-08 20:48:41 -0400

    Added views

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Profile;
+
+class ProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $profile = new Profile();
+        $edit = FALSE;
+        return view('profileForm', ['profile' => $profile,'edit' => $edit  ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->validate([
+            'fname' => 'required',
+            'lname' => 'required',
+            'body' => 'required',
+        ], [
+            'fname.required' => ' First is required',
+            'lname.required' => ' Last is required',
+            'body.required' => ' Body is required',
+        ]);
+        $input = request()->all();
+        $profile = new Profile($input);
+        $profile->user()->associate(Auth::user());
+        $profile->save();
+        return redirect()->route('home')->with('message', 'Profile Created');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = Auth::user();
+        $profile = $user->profile;
+        return view('profile')->with('profile', $profile);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
